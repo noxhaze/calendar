55ae577a4f86f9f8685a07ac1d829803354a5c9d
noxhaze <noxhaze@proton.me>
2023-09-10 21:14:46 -0400

    Implement month class

--- src/DateAPI.ts
+++ src/DateAPI.ts
@@ -1,3 +1,73 @@
 export function getCurrentDateFull(): String {
     return new Date().toDateString();
 }
+
+export class Month {
+    name: String = "";
+    days: Number = 0;
+
+    constructor(cName: String, cDays: Number) {
+        this.name = cName;
+        this.days = cDays;
+    }
+
+    static getMonthFromIndex(index: Number): Month {
+        let month = new Month("", 0);
+        switch (index) {
+            case 0:
+                month = new Month("January", 31);
+                break;
+
+            case 1:
+                month = new Month("February", 28);
+                break;
+
+            case 2:
+                month = new Month("March", 31);
+                break;
+
+            case 3:
+                month = new Month("April", 30);
+                break;
+
+            case 4:
+                month = new Month("May", 31);
+                break;
+
+            case 5:
+                month = new Month("June", 30);
+                break;
+
+            case 6:
+                month = new Month("July", 31);
+                break;
+
+            case 7:
+                month = new Month("August", 31);
+                break;
+
+            case 8:
+                month = new Month("September", 30);
+                break;
+
+            case 9:
+                month = new Month("October", 31);
+                break;
+
+            case 10:
+                month = new Month("November", 30);
+                break;
+
+            case 11:
+                month = new Month("December", 31);
+                break;
+
+            default:
+                console.error("Invalid index for retrieving month");
+                break;
+        }
+        return month;
+    }
+}
+
+
---
+++ src/components/Calendar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<div class="calendar-container">
+</div>
+
+<style>
+    .calendar-container {
+        background-color: var(--tertiary-color);
+        border-radius: 1em;
+        width: 45em;
+        height: 35em;
+    }
+</style>

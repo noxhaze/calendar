353f4fe5886fa86fe106b4bdacf344c613420e5d
noxhaze <noxhaze@proton.me>
2023-09-10 21:16:56 -0400

    Basic day component

---
+++ src/components/Day.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let num: Number;
+</script>
+
+<div class="day-container">
+    <p>{num}</p>
+</div>
+
+<style>
+    .day-container {
+        background-color: var(--primary-color);
+        width: 2em;
+        height: 2em;
+        border-radius: 0.5em;
+    }
+</style>

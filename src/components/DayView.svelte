9202136bae0bd0acd60b0b697bceb1468843004d
noxhaze <noxhaze@proton.me>
2023-09-12 20:39:07 -0400

    Create basic day view component

---
+++ src/components/DayView.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { Day } from "../DateAPI";
+
+    export let day: Day;
+</script>
+
+<div class="day-view">
+    <h2>{day.number}</h2>
+    
+    {#each day.events as event}
+
+    {/each}
+</div>

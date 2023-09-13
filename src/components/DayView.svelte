5c1795c44d9d4e36d877311fbb20b3b76f7fdf40
noxhaze <noxhaze@proton.me>
2023-09-12 20:41:49 -0400

    Display each event inside of day view component

--- src/components/DayView.svelte
+++ src/components/DayView.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { Day } from "../DateAPI";
+    import EventView from "./EventView.svelte";
 
     export let day: Day;
 </script>
@@ -8,6 +9,6 @@
     <h2>{day.number}</h2>
     
     {#each day.events as event}
-
+        <EventView event={event} />
     {/each}
 </div>

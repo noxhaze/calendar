e3c814b1e47b5789c34a85b34c48b50fc10fe798
noxhaze <noxhaze@proton.me>
2023-09-12 20:41:03 -0400

    Setup basic event view component

---
+++ src/components/EventView.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { Event } from "../DateAPI";
+
+    export let event: Event;
+</script>
+
+<div class="event-view">
+    <span>
+        <p class="name">{event.name}</p>
+        <p class="description">{event.description}</p>
+    </span>
+</div>

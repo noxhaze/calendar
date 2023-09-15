04089c874e29c5e9a2d626acf9ade488770957bd
noxhaze <noxhaze@proton.me>
2023-09-15 18:59:49 -0400

    Modal title and more styling

--- src/components/AddEventModal.svelte
+++ src/components/AddEventModal.svelte
@@ -1,7 +1,13 @@
 <div class="add-event-modal-container">
+    <h2 class="title">Add Event</h2>
 </div>
 
 <style>
+    .title {
+        margin-top: 0;
+        color: var(--secondary-color);
+    }
+
     .add-event-modal-container {
         display: block;
         position: fixed;
@@ -10,6 +16,8 @@
 
         left: 50%;
         top: 50%;
+
+        padding: 1em;
 
         transform: translate(-50%, -50%);
         border-radius: 1em;

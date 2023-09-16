52dc41d8d5d82f5adf024aa3e945bbcb0788c21e
noxhaze <noxhaze@proton.me>
2023-09-16 17:03:33 -0400

    Styled event view better

--- src/components/EventView.svelte
+++ src/components/EventView.svelte
@@ -7,6 +7,31 @@
 <div class="event-view">
     <span>
         <p class="name">{event.name}</p>
+        <hr>
         <p class="description">{event.description}</p>
     </span>
 </div>
+
+<style>
+    .event-view {
+        background-color: var(--background-color);
+        border-radius: 1em;
+        padding: 0.5em;
+    }
+
+    hr {
+        margin-left: 0.5em;
+        margin-right: 0.5em;
+        margin-top: -0.5em;
+    }
+
+    .name {
+        font-size: 1em;
+        margin-top: 0;
+        color: var(--primary-color);
+    }
+
+    .description {
+        font-size: 0.75em;
+    }
+</style>

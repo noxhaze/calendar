5dd3bef2a60992aeffc41a822f18a5eae84dc2e1
noxhaze <noxhaze@proton.me>
2023-09-21 19:25:48 -0400

    Save year data to local storage

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,20 @@
     import DayView from "./components/DayView.svelte";
     import Calendar from "./components/Calendar.svelte";
     import { currentDayView, CurrentDayView } from "./stores/DayViewStore";
+    import { currentYear } from "./stores/DateAPIStore";
+    import { Year } from "./DateAPI";
+    import { saveYearToLocalStorage } from "./LocalStorageAPI";
 
     let dayView: CurrentDayView;
+    let year: Year;
 
     currentDayView.subscribe((value) => dayView = value);
+
+    currentYear.subscribe((val) => year = val);
+
+    window.onbeforeunload = () => {
+        saveYearToLocalStorage(year);
+    };
 </script>
 
 <center>
--- src/stores/DateAPIStore.ts
+++ src/stores/DateAPIStore.ts
@@ -1,4 +1,4 @@
 import { writable } from "svelte/store";
 import { Year } from "../DateAPI";
 
-export const currentDayView = writable(new Year(new Date().getFullYear()));
+export const currentYear = writable(new Year(new Date().getFullYear()));

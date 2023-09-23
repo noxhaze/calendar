c9083366ba7bf5c200d06cf3d3daaaf04c62404a
noxhaze <noxhaze@proton.me>
2023-09-23 12:03:14 -0400

    Add current month to store

--- src/App.svelte
+++ src/App.svelte
@@ -18,7 +18,7 @@
     };
 
     function incrementMonth() {
-
+        
     }
 
     function decrementMonth() {
--- src/stores/DateAPIStore.ts
+++ src/stores/DateAPIStore.ts
@@ -1,4 +1,5 @@
 import { writable } from "svelte/store";
-import { retrieveYearFromLocalStorage } from "../LocalStorageAPI";
+import { retrieveMonthFromLocalStorage, retrieveYearFromLocalStorage } from "../LocalStorageAPI";
 
 export const currentYear = writable(retrieveYearFromLocalStorage());
+export const currentMonth = writable(retrieveMonthFromLocalStorage());

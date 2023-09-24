bb6e20eaa0031f1616e514a9475498e86937bd58
noxhaze <noxhaze@proton.me>
2023-09-24 12:30:45 -0400

    Increment/decrement months

--- src/App.svelte
+++ src/App.svelte
@@ -3,26 +3,28 @@
     import DayView from "./components/DayView.svelte";
     import Calendar from "./components/Calendar.svelte";
     import { currentDayView, CurrentDayView } from "./stores/DayViewStore";
-    import { currentYear } from "./stores/DateAPIStore";
+    import { currentMonthIndex, currentYear } from "./stores/DateAPIStore";
     import { Year } from "./DateAPI";
     import { saveYearToLocalStorage } from "./LocalStorageAPI";
 
     let dayView: CurrentDayView;
     let year: Year;
+    let monthIndex: number;
 
     currentDayView.subscribe((value) => dayView = value);
     currentYear.subscribe((val) => year = val);
+    currentMonthIndex.subscribe((val) => monthIndex = val);
 
     window.onbeforeunload = () => {
         saveYearToLocalStorage(year);
     };
 
     function incrementMonth() {
-        
+        currentMonthIndex.set((monthIndex < 11) ? monthIndex++ : 11);
     }
 
     function decrementMonth() {
-
+        currentMonthIndex.set((monthIndex > 0) ? monthIndex-- : 0);
     }
 </script>
 
